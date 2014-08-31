6d57550bbfcfed8d452a9cf8bc0a1db866abae76
BurgerLUA <mattwasnothere@gmail.com>
2014-08-30 22:34:30 -0700

    Added

---
+++ lua/autorun/server/sv_weapon_loadout.lua
@@ -0,0 +1,136 @@
+AddCSLuaFile( "autorun/client/cl_weaponselection.lua" )
+
+--cvar.SetFlags("convarname" , cvar2.GetFlags("convarname") - FCVAR_SERVER_CAN_EXECUTE)
+
+CreateConVar("bur_spawnprotection", "1", FCVAR_REPLICATED + FCVAR_NOTIFY + FCVAR_ARCHIVE , "Enable or disable spawn protection. Values other than 0 enables it " )
+CreateConVar("bur_shieldtime", "3", FCVAR_REPLICATED + FCVAR_NOTIFY + FCVAR_ARCHIVE , "Time it takes for the spawn protection to wear off." )
+CreateConVar("bur_giveweaponsafter", "1", FCVAR_REPLICATED + FCVAR_NOTIFY + FCVAR_ARCHIVE , "Enable or disable giving weapons AFTER spawn protection . Values other than 0 enables it " )
+CreateConVar("bur_spawneffect", "1", FCVAR_REPLICATED + FCVAR_NOTIFY + FCVAR_ARCHIVE , "Enable or disable spawn protection effect. Values other than 0 enables it " )
+
+local folder = "weaponselection"
+
+
+function FirstWeaponSpawn( ply )
+
+	local storename = string.gsub(ply:SteamID(), ":", "_")
+
+	if not file.Exists( folder, "DATA") then
+		file.CreateDir( folder ) 
+		print(folder .. " doesn't exist, creating a new one.")
+	else
+		print(folder .. " exists.")
+	end
+	
+	
+	
+	print("Folder:" .. folder)
+	
+	if not file.Exists( folder.."/"..storename .. ".txt", "DATA" ) then 
+		file.Write( folder.."/"..storename..".txt", "nil nil nil nil nil" )
+		print(folder.."/"..storename .. ".txt doesn't exist, creating a new one.")
+	else	
+		print(folder.."/"..storename .. ".txt exists.")
+	end
+	
+	TrackerString = string.Explode(" ",file.Read(folder.."/"..storename ..".txt"))
+	--PrintTable(TrackerString)
+	ply.Wep = {}
+		
+	for i=1, 5 do
+		ply.Wep[i] = TrackerString[i]
+		ply:SetNWString("Wep"..i,ply.Wep[i])
+	end
+	
+	
+	
+	
+end
+
+hook.Add( "PlayerInitialSpawn", "Load Previous Loadout", FirstWeaponSpawn )
+
+
+function PlayerSpawn(ply)
+
+	ply:StripAmmo()
+	ply:StripWeapons()
+	ply:Give("weapon_physgun")
+	ply:Give("gmod_tool")
+	ply:ConCommand("gm_giveswep weapon_bur_medkit")
+	ply:ConCommand("gm_giveswep fas2_m67")
+
+	if GetConVar("bur_spawnprotection"):GetInt() == 0 or GetConVar("bur_giveweaponsafter"):GetInt() <= 0 then 
+		GiveWeapons(ply)
+	else
+		EnableProtection(ply)
+		
+		timer.Create( ply:Name() .. "removehealth", GetConVar("bur_shieldtime"):GetInt(), 1, function()
+			DisableProtection(ply)
+			if GetConVar("bur_giveweaponsafter"):GetInt() >= 1 then 
+				GiveWeapons(ply)
+			end
+		end)
+
+		if not ply:Alive() then
+			timer.Destroy(ply:Name() .. "removehealth")
+		end
+	end
+end
+
+hook.Add("PlayerSpawn", "Player Spawn", PlayerSpawn)
+
+function GivePlayerAWeapon( ply, cmd, args )
+
+	ply.Wep = {}
+
+	for i=1, 5 do
+		ply.Wep[i] = args[i]
+		ply:SetNWString("Wep"..i,args[i])
+	end
+	
+
+
+	
+	local storename = string.gsub(ply:SteamID(), ":", "_")
+	local folder = "weaponselection"
+
+
+	file.Write( folder.."/"..storename..".txt", ply.Wep[1] .. " " .. ply.Wep[2] .. " " .. ply.Wep[3] .. " " .. ply.Wep[4] .. " " .. ply.Wep[5]  )
+	
+	
+	
+ply:ChatPrint("Your loadout will change next spawn")
+end
+ 
+concommand.Add("weapon_take", GivePlayerAWeapon) --make the console command "weapon_take" run the GivePlayerAWeapon function
+
+
+
+
+function EnableProtection(ply)
+	ply:GodEnable()
+	ply:SetMaterial("Models/effects/splodearc_sheet")
+	if GetConVar("bur_spawneffect"):GetInt() == 1 then 
+		ply:ConCommand("pp_mat_overlay_refractamount 0.1")
+		ply:ConCommand("pp_mat_overlay effects/combine_binocoverlay")
+	end
+end
+
+
+function DisableProtection(ply)
+	ply:GodDisable()
+	ply:SetMaterial("")
+	ply:ConCommand("pp_mat_overlay_refractamount 0")
+	ply:ConCommand("pp_mat_overlay \"\" ")
+	ply:PrintMessage( HUD_PRINTCENTER, "Spawn Protection has worn off!" )
+end
+
+
+function GiveWeapons(ply)
+	if ply.Wep == nil then return end
+	for i=1, 5 do
+		if ply.Wep[i] ~= nil then 
+			ply:ConCommand("gm_giveswep " .. ply.Wep[i])
+		end
+	end
+end
+
---
+++ lua/autorun/weapondata.lua
@@ -0,0 +1,143 @@
+TableEnt = {}
+TableName = {}
+
+TableEnt[1] = {}
+TableEnt[2] = {}
+TableEnt[3] = {}
+TableEnt[4] = {}
+TableEnt[5] = {}
+
+TableName[1] = {}
+TableName[2] = {}
+TableName[3] = {}
+TableName[4] = {}
+TableName[5] = {}
+
+
+TableEnt[1][0] = "nil"
+TableEnt[1][1] = "fas2_deagle"
+TableEnt[1][2] = "fas2_glock20"
+TableEnt[1][3] = "fas2_m1911"
+TableEnt[1][4] = "fas2_ots33"
+TableEnt[1][5] = "fas2_p226"
+TableEnt[1][6] = "fas2_ragingbull"
+TableEnt[1][7] = "weapon_gpee"
+TableEnt[1][8] = "weapon_ss_colt"
+TableEnt[1][9] = "weapon_ss_colt_dual"
+
+
+
+TableName[1][0] = "None"
+TableName[1][1] = "Desert Eagle"
+TableName[1][2] = "Glock 20"
+TableName[1][3] = "M1911"
+TableName[1][4] = "OTS33"
+TableName[1][5] = "P226"
+TableName[1][6] = "Ragingbull Revolver"
+TableName[1][7] = "Your Penis"
+TableName[1][8] = "Serious Colt Revolver"
+TableName[1][9] = "Serious Colt Revolver X2"
+
+TableEnt[2][0] = "nil"
+TableEnt[2][1] = "fas2_mp5a5"
+TableEnt[2][2] = "fas2_mp5k"
+TableEnt[2][3] = "fas2_mp5sd6"
+TableEnt[2][4] = "fas2_pp19"
+TableEnt[2][5] = "fas2_uzi"
+TableEnt[2][6] = "fas2_mac11"
+TableEnt[2][7] = "weapon_ss_tommygun"
+TableEnt[2][8] = "weapon_ss_laser"
+
+TableName[2][0] = "None"
+TableName[2][1] = "MP5A5"
+TableName[2][2] = "MP5K"
+TableName[2][3] = "MP5SD6"
+TableName[2][4] = "BIZON PP19"
+TableName[2][5] = "UZI"
+TableName[2][6] = "MAC11"
+TableName[2][7] = "Serious Tommygun"
+TableName[2][8] = "Serious Lasergun"
+
+TableEnt[3][0] = "nil"
+TableEnt[3][1] = "fas2_ak12"
+TableEnt[3][2] = "fas2_ak47"
+TableEnt[3][3] = "fas2_ak74"
+TableEnt[3][4] = "fas2_an94"
+TableEnt[3][5] = "fas2_famas"
+TableEnt[3][6] = "fas2_g36c"
+TableEnt[3][7] = "fas2_galil"
+TableEnt[3][8] = "fas2_m14"
+TableEnt[3][9] = "fas2_rk95"
+TableEnt[3][10] = "fas2_rpk"
+TableEnt[3][11] = "fas2_sg550"
+TableEnt[3][12] = "fas2_sg552"
+TableEnt[3][13] = "fas2_m4a1"
+TableEnt[3][14] = "fas2_g3"
+TableEnt[3][15] = "fas2_sks"
+TableEnt[3][16] = "weapon_ss_minigun"
+TableEnt[3][17] = "weapon_ss_tommygun_dual"
+
+TableName[3][0] = "None"
+TableName[3][1] = "AK-12"
+TableName[3][2] = "AK-47"
+TableName[3][3] = "AK-74"
+TableName[3][4] = "AN-94"
+TableName[3][5] = "FAMAS"
+TableName[3][6] = "G36C"
+TableName[3][7] = "GALIL"
+TableName[3][8] = "M14"
+TableName[3][9] = "RK-95"
+TableName[3][10] = "RPK"
+TableName[3][11] = "SG550"
+TableName[3][12] = "SG552"
+TableName[3][13] = "M4A1"
+TableName[3][14] = "G3A3"
+TableName[3][15] = "SKS"
+TableName[3][16] = "Serious Minigun"
+TableName[3][17] = "Serious Dual Tommy"
+
+TableEnt[4][0] = "nil"
+TableEnt[4][1] = "fas2_toz34"
+TableEnt[4][2] = "fas2_m3s90"
+TableEnt[4][3] = "fas2_rem870"
+TableEnt[4][4] = "fas2_ks23"
+TableEnt[4][5] = "weapon_bur_gl"
+TableEnt[4][6] = "weapon_bur_c4"
+TableEnt[4][7] = "weapon_ss_singleshotgun"
+TableEnt[4][8] = "weapon_ss_doubleshotgun"
+
+
+TableName[4][0] = "None"
+TableName[4][1] = "TOZ-34 Break Action"
+TableName[4][2] = "M3S90 GUN FOR FAGGOTS"
+TableName[4][3] = "Remington-870 Pump Action"
+TableName[4][4] = "KS-23 Pump Action"
+TableName[4][5] = "M79 Sensor Launcher"
+TableName[4][6] = "C4 Explosives"
+TableName[4][7] = "Serious Shotgun"
+TableName[4][8] = "Serious Double Shotgun"
+
+
+TableEnt[5][0] = "nil"
+TableEnt[5][1] = "fas2_m21"
+TableEnt[5][2] = "fas2_m82"
+TableEnt[5][3] = "fas2_sr25"
+TableEnt[5][4] = "fas2_m24"
+TableEnt[5][5] = "weapon_bur_naughty"
+TableEnt[5][6] = "weapon_ss_sniper"
+TableEnt[5][7] = "weapon_ss_knife"
+TableEnt[5][8] = "weapon_ss_grenadelauncher"
+TableEnt[5][9] = "weapon_ss_rocketlauncher"
+TableEnt[5][10] = "weapon_ss_cannon"
+
+TableName[5][0] = "None"
+TableName[5][1] = "M21 Semi-Auto"
+TableName[5][2] = "M28 Anti-Tank"
+TableName[5][3] = "SR-25 Semi-Auto"
+TableName[5][4] = "M24 Bolt Action"
+TableName[5][5] = "Dildo Cannon"
+TableName[5][6] = "Serious Sniper"
+TableName[5][7] = "Serious Knife"
+TableName[5][8] = "Serious GL"
+TableName[5][9] = "Serious Rocket Launcher"
+TableName[5][10] = "Serious Cannon"
